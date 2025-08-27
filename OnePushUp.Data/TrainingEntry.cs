9066f32a200e3af58270a8f3cc570bbffcda2611
marcinmilewski92 <64347771+marcinmilewski92@users.noreply.github.com>
2025-08-27 17:50:02 +0200

    Handle unsupported DateTimeOffset translations

--- OnePushUp.Data/TrainingEntry.cs
+++ OnePushUp.Data/TrainingEntry.cs
@@ -3,7 +3,7 @@ namespace OnePushUp.Data;
 public class TrainingEntry
 {
     public Guid Id { get; set; }
-    public DateTime DateTime { get; set; }
+    public DateTimeOffset DateTime { get; set; }
     public int NumberOfRepetitions { get; set; }
     public Guid UserId { get; set; }
     public User? User { get; set; }
