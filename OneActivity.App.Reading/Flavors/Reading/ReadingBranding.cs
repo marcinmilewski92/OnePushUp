abe26c081f1d483134f94896776f11ae19a777a1
zavebe <marcin.milewski.92@gmail.com>
2025-08-30 00:36:18 +0200

    feat: update branding assets and colors for OneReading application

--- OneActivity.App.Reading/Flavors/Reading/ReadingBranding.cs
+++ OneActivity.App.Reading/Flavors/Reading/ReadingBranding.cs
@@ -5,9 +5,9 @@ namespace OneActivity.App.Reading.Flavors.Reading;
 public class ReadingBranding : IActivityBranding
 {
     public string AppDisplayName => "OneReading";
-    public string LogoPath => "/splash.svg";
-    public string SplashPath => "/splash.svg";
-    public string? PrimaryColor => "#1e88e5";
-    public string? SecondaryColor => "#1565c0";
-    public string? AccentColor => "#fbc02d";
+    public string LogoPath => "/FrotkaReading.svg";
+    public string SplashPath => "/FrotkaReading.svg";
+    public string? PrimaryColor => "#ff0000";
+    public string? SecondaryColor => "#ff0000";
+    public string? AccentColor => "#fdb127";
 }
